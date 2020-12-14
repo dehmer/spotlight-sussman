9155f4c3891ad1bb207a0d02a53e246dbb16abb2
dehmer <horst.dehmer@syncpoint.io>
2020-12-14 13:38:36 +0100

    LUNR search index integration (WIP).

--- spotlight/index.js
+++ spotlight/index.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import * as R from 'ramda'
+import lunr from 'lunr'
 import './spotlight.css'
 import descriptors from '../model/feature-descriptors.json'
 
-const Search = props => {
+const descriptorIndex = {}
+descriptors.forEach(descriptor => descriptorIndex[descriptor.sidc] = descriptor)
+
+const document = descriptor => {
+  const tags = [
+    ...descriptor.dimension ? descriptor.dimension.split(', ') : [],
+    ...descriptor.scope ? descriptor.scope.split(', ') : []
+  ].flat().join(' ')
+
+  if (!descriptor.hierarchy) console.error(descriptor)
+
+  return {
+    sidc: descriptor.sidc,
+    text: descriptor.hierarchy.join(' '),
+    tags
+  }
+}
+
+const index = lunr(function () {
+  this.pipeline.remove(lunr.stemmer)
+  this.searchPipeline.remove(lunr.stemmer)
+  this.metadataWhitelist = ['position']
+
+  this.ref('sidc')
+  this.field('text')
+  this.field('tags')
+
+  descriptors
+    .map(document)
+    .forEach(document => this.add(document))
+})
+
+const Search = ({ initialValue = '', onChange }) => {
+  const handleChange = ({ target }) => onChange(target.value)
+
   return <div className='search-conainer'>
-    <input className='search-input' placeholder='Spotlight Search'></input>
+    <input
+      className='search-input'
+      placeholder='Spotlight Search'
+      value={initialValue}
+      onChange={handleChange}
+    >
+
+    </input>
   </div>
 }
 
@@ -27,7 +69,7 @@ const Card = props => {
   </div>
 }
 
-const entries = () => R.dropLast(1500, descriptors).map(descriptor => {
+const entries = items => items.map(descriptor => {
   const title = R.last(descriptor.hierarchy)
   const description = R.dropLast(1, descriptor.hierarchy).join(' • ')
   const dimension = descriptor.dimension ? descriptor.dimension.split(', ') : []
@@ -45,11 +87,11 @@ const entries = () => R.dropLast(1500, descriptors).map(descriptor => {
   />
 })
 
-const List = props => {
+const List = ({items}) => {
   return (
     <div className='list-container'>
       <ul className='list'>
-        { entries() }
+        { entries(items) }
       </ul>
     </div>
   )
@@ -57,10 +99,23 @@ const List = props => {
 
 
 export const Spotlight = () => {
+  const [filter, setFilter] = React.useState('')
+  const [items, setItems] = React.useState([])
+
+  const handleSearchChange = value => {
+    setFilter(value)
+    if (!value.trim()) return setItems([])
+
+    // TODO: filter possible '*' from value
+    const term = value.split(' ').map(value => `${value}*`).join(' ')
+    const items = index.search(term).map(entry => descriptorIndex[entry.ref])
+    setItems(items)
+  }
+
   return (
     <div className="spotlight">
-      <Search></Search>
-      <List></List>
+      <Search initialValue={filter} onChange={handleSearchChange}></Search>
+      <List items={items}></List>
     </div>
   )
 }
