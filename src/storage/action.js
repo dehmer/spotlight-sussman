33b19ea28a79d8d9f1c470c9b82923c3b18c378a
dehmer <horst.dehmer@syncpoint.io>
2021-01-06 22:12:11 +0100

    multi-op: identify.

--- src/storage/action.js
+++ src/storage/action.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda'
 import Collection from 'ol/Collection'
 import Feature from 'ol/Feature'
 import { getCenter } from 'ol/extent'
@@ -10,6 +11,7 @@ import { options } from '../model/options'
 import { searchIndex } from '../search/lunr'
 import { readGeometry, readFeature } from './format'
 import * as TS from '../map/ts'
+import selection from '../model/selection'
 
 const option = id => options[id.split(':')[0]](id)
 export const highlightedFeatures = new Collection()
@@ -56,48 +58,53 @@ emitter.on(`:id(${FEATURE_ID})/panto`, txn((storage, { id }) => {
   emitter.emit('map/panto', { center })
 }))
 
-emitter.on(`:id(${LAYER_ID})/identify/down`, ({ id }) => {
-  const geometries = getContainedFeatures(id)
-    .map(readFeature)
-    .map(feature => feature.getGeometry())
-    .map(TS.read)
-  const collection = TS.collect(geometries)
-  const bounds = TS.minimumRectangle(collection)
-  highlightedFeatures.push(new Feature({ geometry: TS.write(bounds) }))
+const geometry = R.cond([
+  [isLayer, id => {
+    const geometries = getContainedFeatures(id)
+      .map(readFeature)
+      .map(feature => feature.getGeometry())
+      .map(TS.read)
+    const collection = TS.collect(geometries)
+    return TS.write(TS.minimumRectangle(collection))
+  }],
+  [isFeature, id => {
+    const item = storage.getItem(id)
+    const feature = readFeature(item)
+    const geometry = TS.read(feature.getGeometry())
+    const bounds = feature.getGeometry().getType() === 'Polygon'
+      ? geometry
+      : TS.minimumRectangle(geometry)
+    return TS.write(bounds)
+  }],
+  [isPlace, id => {
+    const item = storage.getItem(id)
+    return readGeometry(item.geojson)
+  }],
+  [isGroup, id => {
+    const item = storage.getItem(id)
+    const geometries = searchIndex(item.terms)
+      .filter(({ ref }) => !isGroup(ref) && !isSymbol(ref))
+      .map(({ ref }) => ref)
+      .filter(id => isLayer(id) || isFeature(id))
+      .flatMap(id => isLayer(id) ? getContainedFeatures(id) : storage.getItem(id))
+      .map(readFeature)
+      .map(feature => feature.getGeometry())
+      .map(TS.read)
+    const collection = TS.collect(geometries)
+    return collection.getNumGeometries()
+      ? TS.write(TS.minimumRectangle(collection))
+      : null
+  }],
+  [R.T, R.always(null)]
+])
+
+emitter.on(`:id(.*)/identify/down`, ({ id }) => {
+  R.uniq([id, ...selection.selected()])
+    .map(geometry)
+    .filter(R.identity)
+    .forEach(geometry => highlightedFeatures.push(new Feature({ geometry })))
 })
 
-emitter.on(`:id(${FEATURE_ID})/identify/down`, ({ id }) => {
-  const item = storage.getItem(id)
-  const feature = readFeature(item)
-  const geometry = TS.read(feature.getGeometry())
-  const bounds = feature.getGeometry().getType() === 'Polygon'
-    ? geometry
-    : TS.minimumRectangle(geometry)
-  highlightedFeatures.push(new Feature({ geometry: TS.write(bounds) }))
-})
-
-emitter.on(`:id(${PLACE_ID})/identify/down`, ({ id }) => {
-  const item = storage.getItem(id)
-  const geometry = readGeometry(item.geojson)
-  const feature = new Feature({ geometry })
-  highlightedFeatures.push(feature)
-})
-
-emitter.on(`:id(${GROUP_ID})/identify/down`, ({ id }) => {
-  const item = storage.getItem(id)
-  const geometries = searchIndex(item.terms)
-    .filter(({ ref }) => !isGroup(ref))
-    .filter(({ ref }) => !isSymbol(ref))
-    .map(({ ref }) => ref)
-    .filter(id => isLayer(id) || isFeature(id))
-    .flatMap(id => isLayer(id) ? getContainedFeatures(id) : storage.getItem(id))
-    .map(readFeature)
-    .map(feature => feature.getGeometry())
-    .map(TS.read)
-  const collection = TS.collect(geometries)
-  const bounds = TS.minimumRectangle(collection)
-  highlightedFeatures.push(new Feature({ geometry: TS.write(bounds) }))
-})
 
 emitter.on(`:dontcare(.*)/identify/up`, () => {
   highlightedFeatures.clear()
